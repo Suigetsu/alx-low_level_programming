78c7cb8539a9c5c86edfd953acf0345acff753ea
Suigetsu <lagmed.019@gmail.com>
2023-06-15 10:36:12 -0700

    printf this time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - another function using printf this time
+ * Return : 0 again if everything is good
+ */
+
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

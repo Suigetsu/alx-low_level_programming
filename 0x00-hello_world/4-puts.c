d79963c3bc9b85b6c269fed4498844ed4fbf4446
Suigetsu <lagmed.019@gmail.com>
2023-06-15 10:32:31 -0700

    first programyay

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A simple function that prints a random message
+ * Return: on success the main function returns 0, non zero otherwise
+ */
+
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

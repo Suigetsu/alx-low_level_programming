95ce6a018ff96a98cb199103720d7a0aa1f50217
Suigetsu <lagmed.019@gmail.com>
2023-06-19 11:32:12 -0700

    ex9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printing combos
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i++);
+		if (i == 58)
+			break ;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

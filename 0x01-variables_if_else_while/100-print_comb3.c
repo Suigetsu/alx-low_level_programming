a62bbab74ea75483b1087aad4180379191392468
Suigetsu <lagmed.019@gmail.com>
2023-06-19 12:00:11 -0700

    ex10 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+	int	j;
+
+	i = 48;
+	j = i + 1;
+	while (i < j)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == '8')
+				break;
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

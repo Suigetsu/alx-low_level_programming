f1db2be666055fe22dce94bc95bd204e7b2beb23
Suigetsu <lagmed.019@gmail.com>
2023-06-20 12:49:14 -0700

    ex10 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @nb1: first number to add
+ * @nb2: the second one
+ */
+
+int	add(int nb1, int nb2)
+{
+	return (nb1 + nb2);
+}

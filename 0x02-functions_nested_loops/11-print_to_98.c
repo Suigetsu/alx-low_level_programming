8bce08b0d1fc1f764b8e49b9b87e7ec4a81d5b86
Suigetsu <lagmed.019@gmail.com>
2023-06-20 13:07:44 -0700

    ex11 done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * my_putnbr - my function that prints any number
+ * @n: the number I wanna print
+ */
+
+void	my_putnbr(int n)
+{
+	char	s[] = "0123456789";
+
+	if (n >= 0 && n <= 9)
+		_putchar(s[n]);
+	else if (n > 9)
+	{
+		my_putnbr(n / 10);
+		my_putnbr(n % 10);
+	}
+}
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: the starting point
+ */
+
+void	print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 99)
+		{
+			my_putnbr(n);
+			n++;
+			if (n == 99);
+				break;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		while (n > 97)
+		{
+			my_putnbr(n--);
+			if (n == 97)
+				break;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}

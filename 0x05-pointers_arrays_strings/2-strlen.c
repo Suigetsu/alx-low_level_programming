26ffc24b80d5ad112176f43235fd7bd2f6182ea5
Suigetsu <lagmed.019@gmail.com>
2023-06-26 17:14:24 -0700

    ex02 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * a function that returns the length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}

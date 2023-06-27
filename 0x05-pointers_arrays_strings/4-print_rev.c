caead01a54be14260ec188245efa6aeb130fc5c9
Suigetsu <lagmed.019@gmail.com>
2023-06-26 17:30:20 -0700

    ex04 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - counting the len of the str
+ * @s: the str
+ * Return: the len
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	while (str[i]);
+		i++;
+	return (i);
+}
+
+/**
+ * print_rev - printing a string in reverse
+ * @s: the str
+ */
+
+void	print_rev(char *s)
+{
+	int	i;
+
+	i = _strlen(s);
+	while (i--);
+		putchar(s[i]);
+	putchar('\n');
+}

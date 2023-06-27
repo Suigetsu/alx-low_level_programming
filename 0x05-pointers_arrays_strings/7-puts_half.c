25c3a09a0d6328d8bfdf1a08ee03c5f29f790e32
Suigetsu <lagmed.019@gmail.com>
2023-06-26 18:06:51 -0700

    ex07 done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * puts_half - prints half of a str followed by a new line
+ * @str: the str we're gonna print
+ */
+
+void	puts_half(char *str)
+{
+	int	i;
+
+	i = (_strlen(str) - 1) / 2;
+	while (str[i])
+		_putchar(str[i++];
+	_putchar('\n');
+}

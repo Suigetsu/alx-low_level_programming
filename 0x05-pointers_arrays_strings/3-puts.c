dead07764e441c08ce56419b3c418488c0f2e4e5
Suigetsu <lagmed.019@gmail.com>
2023-06-26 17:18:59 -0700

    ex03 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string to stdout
+ * @str: the string
+ */
+
+void	_puts(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		write (1, &s[i++], 1);
+}

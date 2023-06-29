a97d11da94e3b68c59e9f24c4a7598bcd74d8706
Suigetsu <lagmed.019@gmail.com>
2023-06-29 13:09:29 -0700

    ex01 done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _strncat - a function that concatenates two strings depends on n
+ * @dest: the destination
+ * @src: the source
+ * @n: number of bytes we will concat
+ * Return: the destination
+ */
+
+char	*_strncat(char *dest, char *src, int n)
+{
+	int	len;
+	int	i;
+
+	len = _strlen(dest);
+	i = 0;
+	while (i < n)
+		dest[len++] = src[i++];
+	dest[len] = '\0';
+	return (dest);
+}

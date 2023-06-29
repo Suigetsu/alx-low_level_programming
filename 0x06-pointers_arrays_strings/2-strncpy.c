e471832112dfdd58f451c69c5edee48af4a43d2e
Suigetsu <lagmed.019@gmail.com>
2023-06-29 13:31:34 -0700

    testing

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,10 @@ char	*_strncpy(char *dest, char *src, int n)
 	int	i;
 
 	i = 0;
-	while (i < n && src[i])
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }

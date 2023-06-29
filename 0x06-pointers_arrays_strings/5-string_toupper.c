9a214d733c14478c022e721cf80c40f6f220441a
Suigetsu <lagmed.019@gmail.com>
2023-06-29 16:17:15 -0700

    ex05 done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ * @c: the character that we will check
+ * Return: 1 if true, 0 if not
+ */
+
+int	_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: the string
+ * Return: all uppercase string
+ */
+
+char	*string_toupper(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (_islower(str[i])
+			str[i] -=32;
+		i++;
+	}
+	return (str);
+}

b7843317f884600332c683680d49e457766ed099
Suigetsu <lagmed.019@gmail.com>
2023-06-29 13:14:27 -0700

    fixed a problem

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -31,7 +31,7 @@ char	*_strncat(char *dest, char *src, int n)
 
 	len = _strlen(dest);
 	i = 0;
-	while (i < n)
+	while (i < n && src[i])
 		dest[len++] = src[i++];
 	dest[len] = '\0';
 	return (dest);

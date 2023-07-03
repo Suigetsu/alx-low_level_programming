dcf6153172101734ab68ed06d2a8b523d2621b26
Suigetsu <lagmed.019@gmail.com>
2023-07-03 16:01:18 -0700

    first time creating my own strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,8 +4,9 @@
 # include <unistd.h>
 # include <stdio.h>
 
-char	*_memset(char *s, char b, unsigned int n);
-char	*_memcpy(char *dest, char *src, unsigned int n);
-char	*_strchr(char *s, char c);
+char			*_memset(char *s, char b, unsigned int n);
+char			*_memcpy(char *dest, char *src, unsigned int n);
+char			*_strchr(char *s, char c);
+unsigned int	_strspn(char *s, char *accept);
 
 #endif

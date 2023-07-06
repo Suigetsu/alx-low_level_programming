f8f9b1c12a5b97188e73c89c72189607178b2a69
Suigetsu <lagmed.019@gmail.com>
2023-07-06 09:09:02 -0700

    ex01 without static var

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,36 +1,17 @@
 #include "main.h"
 
 /**
- * _strlen - a function that returns the length of a string
- * @s: the string
- * Return: the length
- */
-
-int	_strlen(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-/**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: Pointer to the string to be printed in reverse.
  */
 
 void	_print_rev_recursion(char *s)
 {
-	static int	i;
+	int	i;
 
-	if (i == 0 && _strlen(s) > 1)
-		i = _strlen(s) - 1;
-	if (i < 0)
+	i = 0;
+	if (!s[i])
 		return;
-	_putchar(s[i--]);
-	if (i == 0)
-		_putchar(s[i--]);
-	_print_rev_recursion(s);
+	_print_rev_recursion(&s[i + 1]);
+	_putchar(s[i]);
 }

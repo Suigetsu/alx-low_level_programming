f7b1d1fbb2e546688c96687d41331f1cc3c91425
Suigetsu <lagmed.019@gmail.com>
2023-07-06 07:49:36 -0700

    puts recursed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,8 +10,8 @@ void	_puts_recursion(char *s)
 	int	i;
 
 	i = 0;
-	if (s[i])
-		_putchar(s[i++]);
-	else
-		_putchar('\n');
+	if (!s[i])
+		return (_putchar('\n'));
+	_putchar(s[i++]);
+	_puts_recursion(&s[i]);
 }

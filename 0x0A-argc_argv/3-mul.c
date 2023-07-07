cf7fbf75596e0ddff219648e8170192d273c8ca8
Suigetsu <lagmed.019@gmail.com>
2023-07-07 02:56:40 -0700

    ex03 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @ac: arg count
+ * @av: arg vector
+ * Return: 0, 1 if there is an error
+ */
+
+int	main(int ac, char **av)
+{
+	if (ac <= 2)
+		return (printf("Error\n"), 1);
+	printf("%d\n", atoi(av[1]) * atoi(av[2]));
+	return (0);
+}

59d0590931a2d8b26bb626fbae404618755669ac
Suigetsu <lagmed.019@gmail.com>
2023-07-27 09:26:16 -0700

    bonus done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * print_first - Print "You're beat! and yet, you must allow,"
+ *               "I bore my house upon my back!" before main.
+ *
+ * Description:
+ *    Print the specified message before the main function is executed.
+ */
+
+void	print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

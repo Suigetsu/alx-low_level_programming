2a7d24e015463e713705f17893f32d5a24692311
Suigetsu <lagmed.019@gmail.com>
2023-08-01 12:51:19 -0700

    never forget to use protection

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,6 +40,8 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx > len + 1)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 	new_node->n = n;
 	if (!idx)
 	{
